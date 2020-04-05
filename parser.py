be0c9228ade7e0d90512b6e222ceaaa61d820053
Fred Lahde <frederick.lahde@worldiety.de>
2020-04-05 15:31:53 +0200

    add key parser

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import sys
+import json
 
 def get_child(parent, name):
     for child in parent:
@@ -11,12 +12,12 @@ class Token:
     def __init__(self, word, lemma, begin, end, pos):
         self.word = word
         self.lemma = lemma
-        self.begin = begin
-        self.end = end
+        self.begin = int(begin)
+        self.end = int(end)
         self.pos = pos
 
     def __repr__(self):
-        return self.word
+        return "[%d, %d]: %s" % (self.begin, self.end, self.word)
 
 def parse_token(token_elem):
     word = get_child(token_elem, 'word').text
@@ -61,3 +62,23 @@ class HulthParser:
             sent = parse_sentence(sent)
             sentences.append(sent)
         return sentences
+
+class KeyParser():
+    def __init__(self, f):
+        with open(f, 'r') as fd:
+            self.keys = json.loads(fd.read())
+
+    def get_keyword_phrases(self, idx):
+        ret = []
+        for k in self.keys[idx]:
+            for kk in k:
+                ret.append(kk)
+        return ret
+
+    def get_keywords(self, idx):
+        ret = []
+        for k in self.keys[idx]:
+            for kk in k:
+                for kkk in kk.split(' '):
+                    ret.append(kkk)
+        return ret
